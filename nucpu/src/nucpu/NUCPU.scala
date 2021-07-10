e88addd88aa63fd09fdb2cd81a57304342e69aa9
singularity <chency_singularity@163.com>
2021-07-10 16:37:03 +0800

    env: init file

---
+++ nucpu/src/nucpu/NUCPU.scala
@@ -0,0 +1,9 @@
+package nucpu
+
+import chisel3._
+
+class NUCPU extends Module {
+  val io = IO(new Bundle() {
+
+  })
+}
---
+++ nucpu/tests/src/nucpu/testers/NUCPUTest.scala
@@ -0,0 +1,7 @@
+package nucpu.testers
+
+import chisel3._
+import chiseltest._
+import chiseltest.experimental.TestOptionBuilder._
+import chiseltest.internal.WriteVcdAnnotation
+import nucpu._
