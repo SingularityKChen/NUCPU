2e23c8d24f7f4b08903c5c4eb37af5147e37e348
singularity <chency_singularity@163.com>
2021-07-27 21:08:37 +0800

    feat: add IMM decode
    + now able to get correct imm in different inst type except Z;

--- nucpu/src/nucpu/IDStage.scala
+++ nucpu/src/nucpu/IDStage.scala
@@ -24,11 +24,27 @@ class IDStage()(implicit val p: Configs) extends Module {
   protected val rd: UInt = io.inst(11, 7)
   protected val rs1: UInt = io.inst(19, 15)
   protected val rs2: UInt = io.inst(24, 20)
-  // FIXME: correct the imm via different types
-  protected val imm: UInt = io.inst(31, 20)
   protected val instDecoder: UInt = decoder(minimizer = EspressoMinimizer, input = io.inst, truthTable = rv64ITruthTable)
   protected val instCtrlWires: InstCtrlIOs = Wire(new InstCtrlIOs)
   instCtrlWires.connectDecoder(instDecoder)
+  protected val immSign: UInt = io.inst(31).asUInt()
+  protected val imm30_20: UInt = Mux(instCtrlWires.sel_imm === s"b$IMM_U".U, io.inst(30, 20), immSign)
+  protected val imm19_12: UInt = Mux(instCtrlWires.sel_imm === BitPat("b01?"), io.inst(19, 12), immSign)
+  // TODO: IMM_Z
+  protected val imm11: UInt = MuxLookup(instCtrlWires.sel_imm, immSign, Array(
+    s"b$IMM_UJ".U -> io.inst(20),
+    s"b$IMM_SB".U -> io.inst(7),
+    s"b$IMM_U".U -> 0.U,
+  ))
+  protected val imm10_5: UInt = Mux(instCtrlWires.sel_imm === s"b$IMM_U".U, 0.U, io.inst(30, 25))
+  protected val imm4_1: UInt = Mux(instCtrlWires.sel_imm === BitPat("b00?"),
+    io.inst(11, 8),
+    Mux(instCtrlWires.sel_imm === BitPat("b01?"), io.inst(24, 21), 0.U)
+  )
+  protected val imm0: UInt = Mux(instCtrlWires.sel_imm === s"b$IMM_I".U,
+    io.inst(20),
+    Mux(instCtrlWires.sel_imm === s"b$IMM_S".U, io.inst(7), 0.U)
+  )
   io.alu_fn := instCtrlWires.alu_fn
   io.sel_alu1 := instCtrlWires.sel_alu1
   io.sel_alu2 := instCtrlWires.sel_alu2
@@ -39,5 +55,5 @@ class IDStage()(implicit val p: Configs) extends Module {
   io.rd_w_ena := instCtrlWires.wxd
   //FIXME
   io.rd_w_addr := rd
-  io.imm_data := Cat(Fill(52, imm(11)), imm)
+  io.imm_data := Cat(Fill(p.busWidth - 31, immSign), imm30_20, imm19_12, imm11, imm10_5, imm4_1, imm0)
 }
