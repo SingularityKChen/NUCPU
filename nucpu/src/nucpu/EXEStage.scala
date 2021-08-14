4111546464d0ddd039d8bf770da7394023d901b4
singularity <chency_singularity@163.com>
2021-08-14 22:00:05 +0800

    fix: sraw, srlw
    + fix the rs1 chuck problem;

--- nucpu/src/nucpu/EXEStage.scala
+++ nucpu/src/nucpu/EXEStage.scala
@@ -8,13 +8,19 @@ class EXEStage()(implicit val p: Configs) extends Module {
   val io: EXEStageIOs = IO(new EXEStageIOs())
   override val desiredName = "exe_stage"
   protected val alu: ALU = Module(new ALU())
+  protected val isSRA: Bool = io.aluFn === ("b" + FN_SRA).U
+  protected val isSR: Bool = io.aluFn === ("b" + FN_SR).U
   // True then only take the lsb 32bit of rs1 data
-  protected val rs1Only32: Bool = !io.aluDW && ((io.aluFn === ("b" + FN_SR).U) || (io.aluFn === ("b" + FN_SRA).U))
-  protected val rs1DataWire: UInt = Mux(rs1Only32,
-    Cat(Fill(p.busWidth - 32, io.rs1Data(31)), io.rs1Data(31, 0)), io.rs1Data
+  protected val rs1Only32: Bool = !io.aluDW && (isSR || isSRA)
+  // When the results will be signed extended, keep the sign bit. Otherwise, chuck with zeros.
+  protected val rs1Fill: UInt = Mux(isSRA,
+    Cat(Fill(p.busWidth - 32, io.rs1Data(31))),
+    0.U((p.busWidth - 32).W)
+  )
+  protected val rs1DataWire: UInt = Mux(rs1Only32, Cat(rs1Fill, io.rs1Data(31, 0)), io.rs1Data
   )
   // True then only take the lsb 5 bit of rs2 data
-  protected val rs2Only5: Bool = !io.aluDW && ((io.aluFn === ("b" + FN_SL).U) || (io.aluFn === ("b" + FN_SRA).U))
+  protected val rs2Only5: Bool = !io.aluDW && (isSR || (io.aluFn === ("b" + FN_SL).U) || isSRA)
   protected val rs2DataWire: UInt = Mux(rs2Only5,
     Cat(0.U((p.busWidth - 5).W), io.rs2Data(4, 0)), io.rs2Data
   )
