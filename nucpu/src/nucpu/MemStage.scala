478fb2a5a744af68bcdbbc412b989ba8c4e2aaed
singularity <chency_singularity@163.com>
2021-08-14 23:11:02 +0800

    refactor: move mem stage IOs into NUCPUIOs.scala;

--- nucpu/src/nucpu/MemStage.scala
+++ nucpu/src/nucpu/MemStage.scala
@@ -8,22 +8,7 @@ import chisel3.util.experimental.decode._
 import scala.math.pow
 
 class MemStage()(implicit val p: Configs) extends Module {
-  val io = IO(new Bundle() {
-    val exeAddr: UInt = Input(UInt(p.busWidth.W))
-    val exeData: UInt = Input(UInt(p.busWidth.W))
-    val func3: UInt = Input(UInt(3.W))
-    val memCmd: UInt = Input(UInt(M_X.length.W))
-    val exeMemValid: Bool = Input(Bool())
-    // To read memory
-    val memAddr: UInt = Output(UInt(p.busWidth.W))
-    val memRData: UInt = Input(UInt(p.busWidth.W))
-    val memDoWrite: Bool = Output(Bool())
-    val memWData: UInt = Output(UInt(p.busWidth.W))
-    val memMask: UInt = Output(UInt(p.busWidth.W))
-    val memValid: Bool = Output(Bool())
-    // To Write Back
-    val wbData: UInt = Output(UInt(p.busWidth.W))
-  })
+  val io: MemStageIOs = IO(new MemStageIOs())
   protected def getMaskStr(func3: Int, addr: Int): String = {
     val str = "b"
     val len = pow(2, func3 + 3).toInt
