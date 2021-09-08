79f7a152300638749d19605147715ac23e28c0e3
singularity <chency_singularity@163.com>
2021-09-08 23:48:56 +0800

    feat: use official bin for regression
    + modify all regression test dir;
    + add color for the info;
    + able to interrupt the tests;

--- regression.pl
+++ regression.pl
@@ -4,16 +4,21 @@ use warnings;
 use Getopt::Long;
 use Cwd qw(getcwd);
 use File::Find;
+use Term::ANSIColor qw(:constants);
+
 my $root_dir = getcwd;
 $| = 1;
-my ($regression, $riscv_test, $cpu_test, $am_test, $mario_test, $update_build);
+my ($regression, $riscv_test, $cpu_test, $am_test, $mario_test, $interrupt_test);
 # AM Tests
 my ($time_test, $yield_test, $hello_test);
 my $AM_dir = $root_dir."/AM";
 my $oscpu_bin_dir = $AM_dir."/oscpu-framework/bin";
 my $riscv_test_dir = $oscpu_bin_dir."/non-output/riscv-tests";
 my $cpu_test_dir = $oscpu_bin_dir."/non-output/cpu-tests";
-my $am_test_dir = $AM_dir."/am-kernels/tests/am-tests/build";
+my $hello_test_dir = $oscpu_bin_dir."/custom-output/hello";
+my $yield_test_dir = $oscpu_bin_dir."/custom-output/yield-test";
+my $time_test_dir = $oscpu_bin_dir."/custom-output/time-test";
+my $interrupt_test_dir = $oscpu_bin_dir."/custom-output/interrupt-test";
 my $mario_test_dir = $oscpu_bin_dir."/custom-output/mario";
 #
 my $waveform_filename = $root_dir."/build/NUCPU.vcd";
@@ -27,13 +32,14 @@ if (-e $regression_log_dir) {
 }
 
 GetOptions (
-    'regression|r'  => \$regression,
-    'rsicvtest|v'   => \$riscv_test,
-    'cputest|c'     => \$cpu_test,
-    'amtest|a'      => \$am_test,
-    'mario|m'       => \$mario_test,
-    'timetest|t'    => \$time_test,
-    'yieldtest|y'   => \$yield_test,
+    'regression|r' => \$regression,
+    'rsicvtest|v'  => \$riscv_test,
+    'cputest|c'    => \$cpu_test,
+    'amtest|a'     => \$am_test,
+    'mario|m'      => \$mario_test,
+    'timetest|t'   => \$time_test,
+    'yieldtest|y'  => \$yield_test,
+    'interrupt|i'  => \$interrupt_test,
 );
 
 if (defined $regression) {
@@ -46,20 +52,12 @@ if (defined($am_test)) {
     $time_test = 1;
     $yield_test = 1;
     $hello_test = 1;
+    $interrupt_test = 1;
 }
-my ($time_test_arg, $yield_test_arg, $hello_test_arg);
-if (defined($time_test)) {
-    $time_test_arg = "t";
-}
-if (defined($yield_test)) {
-    $yield_test_arg = "i";
-}
-if (defined($hello_test)) {
-    $hello_test_arg = "h";
-}
+
 my @failed_tests;
 if (defined($riscv_test) || defined($cpu_test) || defined($am_test) ||
-    defined($mario_test) || defined($time_test) || defined($yield_test) || defined($hello_test)) {
+    defined($mario_test) || defined($time_test) || defined($yield_test) || defined($hello_test) || defined($interrupt_test)) {
     # update RTL
     my $update_RTL = system("cd $root_dir; mill -i __.nucpu.tests.runMain nucpu.testers.Generator");
     # update EMU
@@ -73,12 +71,17 @@ if (defined($riscv_test) || defined($cpu_test) || defined($am_test) ||
         if (defined($cpu_test)) {
             do_regression_tests($cpu_test_dir);
         }
-        foreach my $am_test_arg ($time_test_arg, $hello_test_arg, $yield_test_arg) {
-            if (defined($am_test_arg)) {
-                printf("[INFO] AM Test $am_test_arg.\n");
-                system("cd $am_test_dir/../; make clean; make ARCH=riscv64-mycpu mainargs=$am_test_arg");
-                do_regression_tests($am_test_dir, $am_test_arg);
-            }
+        if (defined($hello_test)) {
+            do_regression_tests($hello_test_dir);
+        }
+        if (defined($time_test)) {
+            do_regression_tests($time_test_dir);
+        }
+        if (defined($yield_test)) {
+            do_regression_tests($yield_test_dir);
+        }
+        if (defined($interrupt_test)) {
+            do_regression_tests($interrupt_test_dir);
         }
         if (defined($mario_test)) {
             do_regression_tests($mario_test_dir);
@@ -93,7 +96,7 @@ if (defined($riscv_test) || defined($cpu_test) || defined($am_test) ||
         close(FAILEDFILES);
     }
     else {
-        printf("[ERROR] Rebuild RTL or EMU error!\n");
+        print RED, "[ERROR] Rebuild RTL or EMU error!\n", RESET;
     }
 }
 
@@ -114,11 +117,20 @@ sub do_regression_tests {
     my @test_files = get_bin_files($test_dir);
     foreach my $test_file (@test_files) {
         my $test_result = run_test_case($test_file, $postfix);
-        if ($test_result != 0) {
-            if (defined($postfix)) {
-                $test_file = $test_file."_".$postfix;
+        if ($test_result == 0) {
+            printf("[INFO] Test $test_file Passed!\n");
+        }
+        else {
+            if ($test_result == 256) {
+                print RED, "[ERROR] Test $test_file failed!\n", RESET;
+                if (defined($postfix)) {
+                    $test_file = $test_file."_".$postfix;
+                }
+                push(@failed_tests, $test_file);
             }
-            push(@failed_tests, $test_file);
+            else {
+                print BRIGHT_MAGENTA, "\n[WARNING] Test $test_file may fail $test_result!\n", RESET;
+            }
         }
     }
 }
@@ -126,16 +138,13 @@ sub do_regression_tests {
 sub run_test_case {
     my ($test_file, $postfix) = @_;
     my @spl = split(/\//, $test_file);
-    my @spl2 = split(/-riscv64-mycpu.bin/, $spl[-1]);
+    my @spl2 = split(/.bin/, $spl[-1]);
     my $test_name = $spl2[-1];
     if (defined($postfix)) {
         $test_name = $test_name."_".$postfix;
     }
     printf("[INFO] Current Test is $test_name\n");
-    my $test_result = system("$emu_file -i $test_file  --dump-wave -b 0 -e 2000000 > $regression_log_dir/$test_name.log");
-    if ($test_result != 0) {
-        printf("[ERROR] Test $test_file failed!\n");
-    }
+    my $test_result = system("$emu_file -i $test_file --dump-wave -b 0 -e 2000000 > $regression_log_dir/$test_name.log");
     system("mv $waveform_filename $regression_log_dir/$test_name.vcd");
     return($test_result);
 }
